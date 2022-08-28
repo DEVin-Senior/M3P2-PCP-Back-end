33fbedf27b6c15a95fe48e50bef23a48827b0959
Eduardo <eduardoamorim927@gmail.com>
2022-08-28 17:47:40 -0300

    #ALL-BACKEND-02 - Commit inicial

---
+++ src/main/java/com/devinhouse/pcpbackend/PcpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devinhouse.pcpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcpBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devinhouse/pcpbackend/PcpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinhouse.pcpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
