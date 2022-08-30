548e53962c368e3ebff053e4a39bbd983cf7d714
Leônidas Moritz Rosa <leonidas.rosa@senior.com.br>
2022-08-30 20:55:59 -0300

    #SQ02-01 - Ajuste lista de enumerados do docente para entidade relacional 1 para N.

--- src/main/java/com/devinhouse/pcpbackend/models/TeacherEntity.java
+++ src/main/java/com/devinhouse/pcpbackend/models/TeacherEntity.java
@@ -4,12 +4,15 @@ import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
 import javax.persistence.Table;
 
 import com.devinhouse.pcpbackend.enums.SkillEnum;
@@ -39,6 +42,8 @@ public class TeacherEntity {
     @Column(nullable = false)
     private String email;
 
+    @ElementCollection(fetch = FetchType.LAZY, targetClass = SkillEnum.class)
+    @JoinTable(name = "skill")
     @Enumerated(EnumType.STRING)
     private List<SkillEnum> skills;
 
