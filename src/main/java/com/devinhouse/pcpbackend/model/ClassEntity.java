3bc39d859d5aac24acd56179faf9563988a196f5
Guilherme Fortes <guilhermelucianofortes@gmail.com>
2022-08-30 21:28:56 -0300

    SQ03-00 - Criação da entidade Class

---
+++ src/main/java/com/devinhouse/pcpbackend/model/ClassEntity.java
@@ -0,0 +1,37 @@
+package com.devinhouse.pcpbackend.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "turmas")
+public class ClassEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(name = "nome")
+	private String name;
+	@Column(name = "data_inicio")
+	private LocalDate initialDate;
+	@Column(name = "data_fim")
+	private LocalDate endDate;
+	@Column(name = "link_matriz")
+	private String matrixLink;
+	// TODO linkar com módulo
+
+}
