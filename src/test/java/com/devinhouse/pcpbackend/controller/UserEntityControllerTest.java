512150e7836471af9d302f3aabbb7630e7b139e8
all-an <all-an@hotmail.com>
2022-08-31 21:21:06 -0300

    #ALL-BACKEND-08 - Refatorando User para UserEntity

--- src/test/java/com/devinhouse/pcpbackend/controller/UserEntityControllerTest.java
+++ src/test/java/com/devinhouse/pcpbackend/controller/UserEntityControllerTest.java
@@ -1,11 +1,7 @@
 package com.devinhouse.pcpbackend.controller;
 
 import com.devinhouse.pcpbackend.model.UserEntity;
-<<<<<<< HEAD
-import com.devinhouse.pcpbackend.service.UserService;
-=======
 import com.devinhouse.pcpbackend.service.UserEntityService;
->>>>>>> all-backend-08
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
@@ -35,22 +31,16 @@ public class UserEntityControllerTest {
     private static final String URL = "/loginRegister";
 
     @MockBean
-<<<<<<< HEAD
-    UserService service;
-=======
     UserEntityService service;
->>>>>>> all-backend-08
 
     @Autowired
     MockMvc mvc;
 
     @Test
     public void testSave() throws Exception {
-<<<<<<< HEAD
-        BDDMockito.given(service.saveUser(Mockito.any(UserEntity.class))).willReturn(getMockUser());
-=======
+
         BDDMockito.given(service.saveUserEntity(Mockito.any(UserEntity.class))).willReturn(getMockUserEntity());
->>>>>>> all-backend-08
+
 
         mvc.perform(MockMvcRequestBuilders.post(URL).content(getJsonPayload(PASSWORD, EMAIL))
                         .contentType(MediaType.APPLICATION_JSON)
@@ -60,36 +50,14 @@ public class UserEntityControllerTest {
     }
 
     @Test
-<<<<<<< HEAD
-    public void testSaveInvalidUser() throws Exception {
-=======
     public void testSaveInvalidUserEntity() throws Exception {
->>>>>>> all-backend-08
+
         mvc.perform(MockMvcRequestBuilders.post(URL).content(getJsonPayload( "email", PASSWORD))
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().is4xxClientError());
     }
-
-
-<<<<<<< HEAD
-    public UserEntity getMockUser() {
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(ID);
-        userEntity.setPassword(PASSWORD);
-        userEntity.setEmail(EMAIL);
-
-        return userEntity;
-    }
-
-    public String getJsonPayload(String password,  String email) throws JsonProcessingException {
-        UserEntity userEntity = new UserEntity();
-        userEntity.setPassword(password);
-        userEntity.setEmail(email);
-
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.writeValueAsString(userEntity);
-=======
+    
     public UserEntity getMockUserEntity() {
         UserEntity user = new UserEntity();
         user.setId(ID);
@@ -106,7 +74,6 @@ public class UserEntityControllerTest {
 
         ObjectMapper mapper = new ObjectMapper();
         return mapper.writeValueAsString(user);
->>>>>>> all-backend-08
     }
 
 }
