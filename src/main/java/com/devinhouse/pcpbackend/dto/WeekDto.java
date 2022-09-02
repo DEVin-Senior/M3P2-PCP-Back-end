507f1c364db6341aec232d6e3457e577f8676fa4
mzanquet <monique.zanquet@gmail.com>
2022-09-01 21:06:29 -0300

    ALL-BACKEND-06 - Criar entidade e endpoints Semana - Parte 1

---
+++ src/main/java/com/devinhouse/pcpbackend/dto/WeekDto.java
@@ -0,0 +1,25 @@
+package com.devinhouse.pcpbackend.dto;
+
+import com.devinhouse.pcpbackend.model.TeacherEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class WeekDto {
+
+    @NotBlank
+    private String content;
+
+    @NotNull
+    private LocalDate initialDate;
+
+    @NotNull
+    private Long teacherId;
+
+
+}
---
+++ src/main/java/com/devinhouse/pcpbackend/service/WeekService.java
@@ -0,0 +1,37 @@
+package com.devinhouse.pcpbackend.service;
+
+import com.devinhouse.pcpbackend.model.WeekEntity;
+import com.devinhouse.pcpbackend.repository.WeekRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WeekService {
+    private final WeekRepository repository;
+
+    public WeekService(WeekRepository repository) {
+        this.repository = repository;//
+    }
+
+    public WeekEntity insert(WeekEntity week) {
+        return repository.save(week);
+    }
+
+    public WeekEntity update(Long id, WeekEntity week) {
+        WeekEntity updatedWeek = repository.findById(id).get();
+        updatedWeek.setContent(week.getContent());
+        updatedWeek.setInitialDate(week.getInitialDate());
+        return repository.save(updatedWeek);
+    }
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+    public List<WeekEntity> findAll() {
+        return repository.findAll();
+    }
+    public WeekEntity findById(Long weekId) {
+        return repository.findById(weekId).get();
+    }
+
+}
