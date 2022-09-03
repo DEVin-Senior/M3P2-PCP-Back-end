89e12892b4a1fd708824298cffd22fda7988b8ed
Leônidas Moritz Rosa <leonidas.rosa@senior.com.br>
2022-09-03 10:39:07 -0300

    #ALL-BACKEND-10 - Criada classe padrão para lançamento de exceções na camada de Serviço.

--- src/main/java/com/devinhouse/pcpbackend/common/constants/DefaultMessageConstants.java
+++ src/main/java/com/devinhouse/pcpbackend/common/constants/DefaultMessageConstants.java
@@ -1,14 +1,18 @@
 package com.devinhouse.pcpbackend.common.constants;
 
+/**
+ * Classe de enumerados com mensagens padrões para uso no lançamento de exceções.
+ */
 public enum DefaultMessageConstants {
 
     ENTITY_NOT_FOUND("Entity '%s' not found"),
     ENTITY_NOT_FOUND_BY_ID("Entity '%s' with id = '%s' not found"),
-    PARAMS_MISSING("Parameters [%s] is missing."),
-    FIELDS_IS_REQUIRED("Field '%s' is required"),
+    MISSING_PARAMETER("Parameters [%s] is missing."),
+    FIELD_IS_REQUIRED("Field '%s' is required"),
     SECURITY_CONFIGURATION_ERROR("Erro na configuração de segurança"),
-    ERROR_CREATE_OBJECT("Erro ao salvar o %s. Causa: %s"),
+    ERROR_CREATE_OBJECT("Erro ao criar o %s. Causa: %s"),
     ERROR_PERSIST_DATA("Erro ao salvar o %s no banco de dados. Causa: %s"),
+    ERROR_UPDATE_DATA("Erro ao atualizar o %s no banco de dados. Causa: %s"),
     ERROR_DELETE_DATA("Erro ao deletar o %s no banco de dados. Causa: %s"),
     ERROR_DELETE_DATA_BY_ID("Erro ao deletar o %s com ID '%s' no banco de dados. Causa: %s");
 
---
+++ src/main/java/com/devinhouse/pcpbackend/common/exception/ServiceException.java
@@ -0,0 +1,66 @@
+package com.devinhouse.pcpbackend.common.exception;
+
+import java.io.Serial;
+
+import com.devinhouse.pcpbackend.common.DefaultMessageHelper;
+import com.devinhouse.pcpbackend.common.constants.DefaultMessageConstants;
+
+/**
+ * Classe para lançamento de exceções padrões na camada de Serviço.
+ * <p>
+ * Poderá ser utilizada também para lançamento de exceções referente a regra de negócio na camada de Serviço.
+ */
+public class ServiceException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ServiceException(Throwable cause) {
+        super(cause);
+    }
+
+    public ServiceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public ServiceException entityNotFoundException(String entity){
+        return new ServiceException(DefaultMessageHelper.getMessage(DefaultMessageConstants.ENTITY_NOT_FOUND, entity));
+    }
+
+    public ServiceException entityNotFoundByIdException(String entity, String id){
+        return new ServiceException(DefaultMessageHelper.getMessage(DefaultMessageConstants.ENTITY_NOT_FOUND_BY_ID, entity, id));
+    }
+
+    public ServiceException missingParameterException(String parameter){
+        return new ServiceException(DefaultMessageHelper.getMessage(DefaultMessageConstants.MISSING_PARAMETER, parameter));
+    }
+
+    public ServiceException fieldIsRequiredException(String field){
+        return new ServiceException(DefaultMessageHelper.getMessage(DefaultMessageConstants.FIELD_IS_REQUIRED, field));
+    }
+
+    public ServiceException errorPersistDataException(String data){
+        return new ServiceException(DefaultMessageHelper.getMessage(DefaultMessageConstants.ERROR_PERSIST_DATA, data));
+    }
+
+    public ServiceException errorUpdateDataException(String data){
+        return new ServiceException(DefaultMessageHelper.getMessage(DefaultMessageConstants.ERROR_UPDATE_DATA, data));
+    }
+
+    public ServiceException errorDeleteDataException(String data){
+        return new ServiceException(DefaultMessageHelper.getMessage(DefaultMessageConstants.ERROR_DELETE_DATA, data));
+    }
+
+    public ServiceException errorDeleteDataByIdException(String data){
+        return new ServiceException(DefaultMessageHelper.getMessage(DefaultMessageConstants.ERROR_DELETE_DATA_BY_ID, data));
+    }
+
+}
