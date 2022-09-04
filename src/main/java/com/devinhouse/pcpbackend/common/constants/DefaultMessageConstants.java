890f08abefd806bdf30c37afec1a278a121f7771
Leônidas Moritz Rosa <leogui.moritz@gmail.com>
2022-09-04 14:23:06 -0300

    #ALL-BACKEND-13 - Refatorado camada Service da entidade Teacher para correção dos erros nos testes unitários.

--- src/main/java/com/devinhouse/pcpbackend/common/constants/DefaultMessageConstants.java
+++ src/main/java/com/devinhouse/pcpbackend/common/constants/DefaultMessageConstants.java
@@ -6,14 +6,13 @@ package com.devinhouse.pcpbackend.common.constants;
 public enum DefaultMessageConstants {
 
     ENTITY_NOT_FOUND("Entity '%s' not found"),
-    ENTITY_NOT_FOUND_BY_ID("Entity '%s' with id = '%s' not found"),
+    ENTITY_NOT_FOUND_BY_ID("Entity '%s' with id = '%s' not found."),
     MISSING_PARAMETER("Parameters [%s] is missing."),
     FIELD_IS_REQUIRED("Field '%s' is required"),
     SECURITY_CONFIGURATION_ERROR("Erro na configuração de segurança"),
     ERROR_CREATE_OBJECT("Erro ao criar o %s. Causa: %s"),
     ERROR_PERSIST_DATA("Erro ao salvar o %s no banco de dados. Causa: %s"),
-    ERROR_UPDATE_DATA("Erro ao atualizar o %s no banco de dados. Causa: %s"),
-    ERROR_DELETE_DATA("Erro ao deletar o %s no banco de dados. Causa: %s"),
+    ERROR_UPDATE_DATA_BY_ID("Erro ao atualizar o %s com ID %s no banco de dados. Causa: %s"),
     ERROR_DELETE_DATA_BY_ID("Erro ao deletar o %s com ID '%s' no banco de dados. Causa: %s");
 
     private final String message;
