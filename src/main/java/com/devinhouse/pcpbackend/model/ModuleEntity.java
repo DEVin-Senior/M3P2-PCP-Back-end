1aad99ca9f42b16edc74591bc614f4bacdc58cb7
Eduardo <eduardoamorim927@gmail.com>
2022-09-04 09:40:42 -0300

    #ALL-BACKEND-14 - Segunda etapa da refatoração das entidades e configurações de ambiente de produção

--- src/main/java/com/devinhouse/pcpbackend/model/ModuleEntity.java
+++ src/main/java/com/devinhouse/pcpbackend/model/ModuleEntity.java
@@ -6,16 +6,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+
 @Entity
-@Table(name = "module")
+@Table(name = "tb_module")
 public class ModuleEntity {
 
     @Id
@@ -29,17 +28,5 @@ public class ModuleEntity {
             joinColumns = @JoinColumn(name = "module_id"),
             inverseJoinColumns = @JoinColumn(name = "week_id")
     )
-    private List<WeekEntity> weekEntityList = new ArrayList<>();
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof ModuleEntity that)) return false;
-        return getId().equals(that.getId()) && getName().equals(that.getName()) && getWeekEntityList().equals(that.getWeekEntityList());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getName(), getWeekEntityList());
-    }
+    private List<WeekEntity> weekEntityList;
 }
