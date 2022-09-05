8bbc8b22cb241e3bd51cd6200125ca68cf882ebe
Diego_Costa <diegopprog@gmail.com>
2022-09-05 19:02:50 -0300

    Atualização do CourseClass Controller

--- src/main/java/com/devinhouse/pcpbackend/converter/CourseClassConverter.java
+++ src/main/java/com/devinhouse/pcpbackend/converter/CourseClassConverter.java
@@ -6,21 +6,5 @@ import com.devinhouse.pcpbackend.model.ClassEntity;
 
 public class CourseClassConverter {
 	
-	private CourseClassConverter() {}
 	
-	public static ClassEntity converterCourseClass(ClassCreateDto classDto) {
-		
-		ClassEntity courseClass = new ClassEntity();
-		
-		courseClass.setName(classDto.getName());
-		courseClass.setInitialDate(classDto.getInitialDate());
-		courseClass.setEndDate(classDto.getEndDate());
-		courseClass.setStack(classDto.getStack());
-		courseClass.setMatrixLink(classDto.getMatrixLink());
-		courseClass.setArchive(classDto.getArchive());
-		courseClass.setModuleEntityList(classDto.getModuleEntityList());
-		courseClass.setEventEntityList(classDto.getEventEntityList());
-		
-		return courseClass;
-	}
 }
