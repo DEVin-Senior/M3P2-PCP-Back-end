a977f1ded3295bf1996547f3c623c016d679a009
Eduardo <eduardoamorim927@gmail.com>
2022-09-05 19:20:42 -0300

    #SQ03-18 - Configuração incial do modelMapper

--- src/main/java/com/devinhouse/pcpbackend/controller/CourseClassController.java
+++ src/main/java/com/devinhouse/pcpbackend/controller/CourseClassController.java
@@ -1,29 +1,30 @@
 package com.devinhouse.pcpbackend.controller;
 
-import java.util.List;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import com.devinhouse.pcpbackend.dto.ClassCreateDto;
+import com.devinhouse.pcpbackend.dto.ClassReadDto;
+import com.devinhouse.pcpbackend.model.ClassEntity;
+import com.devinhouse.pcpbackend.service.ClassService;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devinhouse.pcpbackend.converter.CourseClassConverter;
-import com.devinhouse.pcpbackend.dto.ClassCreateDto;
-import com.devinhouse.pcpbackend.dto.TeacherDto;
-import com.devinhouse.pcpbackend.model.ClassEntity;
-import com.devinhouse.pcpbackend.model.TeacherEntity;
-import com.devinhouse.pcpbackend.service.ClassService;
+@AllArgsConstructor
 
 @RequestMapping("/turmas")
 @RestController
 public class CourseClassController {
-	
-  
+
+    private ClassService classService;
+    private ModelMapper modelMapper;
+
+    public ClassEntity classCreateDtoForEntity (ClassCreateDto classCreateDto) {
+        return modelMapper.map(classCreateDto, ClassEntity.class);
+    }
+
+    public ClassReadDto classEntityToClassReadDto(ClassEntity classEntity) {
+        return modelMapper.map(classEntity, ClassReadDto.class);
+    }
+
+
 }
