b779a2da24b09f23222dfbc3e6240bb1de78b7f3
Renan Pinho <renan_aguillarpinho@hotmail.com>
2022-09-05 10:24:21 -0300

    SQ03 - 12 - Criação de ClassUpdateDto, ModuleUpdateDto e WeekUpdateDto - UPDATE e Ajustes nos Dtos CREATE e READ.

--- src/main/java/com/devinhouse/pcpbackend/dto/WeekUpdateDto.java
+++ src/main/java/com/devinhouse/pcpbackend/dto/WeekUpdateDto.java
@@ -5,16 +5,24 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class WeekReadDto {
+public class WeekUpdateDto {
 
-    private Long id;
+    @NotBlank(message = "Content não pode estar em branco")
+    @Size(max = 60)
     private String content;
+
+    @NotNull(message = "Data não pode estar em branco")
     private LocalDate initialDate;
+
+    @NotNull(message = "Professor não pode estar em branco")
     private TeacherEntity teacherEntity;
 }
