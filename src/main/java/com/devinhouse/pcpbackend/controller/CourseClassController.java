8296f4a9bef3053938b5903a44008e841a7e052c
all-an <all-an@hotmail.com>
2022-09-06 10:00:37 -0300

    #SQ01-03

---
+++ src/main/java/com/devinhouse/pcpbackend/controller/CourseClassController.java
@@ -0,0 +1,80 @@
+package com.devinhouse.pcpbackend.controller;
+
+import com.devinhouse.pcpbackend.converter.WeekConverter;
+import com.devinhouse.pcpbackend.dto.ClassCreateDto;
+import com.devinhouse.pcpbackend.dto.ClassReadDto;
+import com.devinhouse.pcpbackend.dto.ClassUpdateDto;
+import com.devinhouse.pcpbackend.dto.WeekDto;
+import com.devinhouse.pcpbackend.model.ClassEntity;
+import com.devinhouse.pcpbackend.model.WeekEntity;
+import com.devinhouse.pcpbackend.service.ClassService;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@AllArgsConstructor
+
+@RequestMapping("/turmas")
+@RestController
+public class CourseClassController {
+
+    private ClassService classService;
+    private ModelMapper modelMapper;
+
+    public ClassEntity classCreateDtoForEntity (ClassCreateDto classCreateDto) {
+        return modelMapper.map(classCreateDto, ClassEntity.class);
+    }
+
+    public ClassReadDto classEntityToClassReadDto(ClassEntity classEntity) {
+        return modelMapper.map(classEntity, ClassReadDto.class);
+    }
+    
+    public ClassEntity classEntityReadForEditDto(ClassUpdateDto classUpdateDto) {
+        return modelMapper.map(classUpdateDto, ClassEntity.class);
+    }
+    
+    public ClassUpdateDto classEntityEditDto(ClassEntity classEntity) {
+        return modelMapper.map(classEntity, ClassUpdateDto.class);
+    }
+    
+    @PostMapping
+    public void save (@RequestBody ClassCreateDto classCreateDto) {
+        var classEntity = classCreateDtoForEntity(classCreateDto);
+        classService.save(classEntity);
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<ClassEntity>> getAllClasses () {
+        return ResponseEntity.ok().body(classService.getAll());
+    }
+    
+    @PutMapping
+    public void update(@RequestBody ClassCreateDto createDto, @PathVariable Long id) {
+    	
+    }
+
+    @PutMapping("allocation/{idClass}/{idWeek}/{idTeacher}")
+    public ClassUpdateDto allocation(@PathVariable Long idClass, @PathVariable Long idWeek, @PathVariable Long idTeacher) {
+        return classService.allocatteTeacher(idClass, idWeek, idTeacher);
+    }
+
+    @GetMapping("/{id}")
+    public ClassUpdateDto findById(@PathVariable Long id) {
+        return classService.findById(id);
+    }
+
+
+}
--- src/main/java/com/devinhouse/pcpbackend/service/ClassService.java
+++ src/main/java/com/devinhouse/pcpbackend/service/ClassService.java
@@ -1,7 +1,25 @@
 package com.devinhouse.pcpbackend.service;
 
+import com.devinhouse.pcpbackend.common.DefaultMessageHelper;
+import com.devinhouse.pcpbackend.common.constants.DefaultMessageConstants;
+import com.devinhouse.pcpbackend.common.exception.ApiException;
+import com.devinhouse.pcpbackend.dto.ClassReadDto;
+import com.devinhouse.pcpbackend.dto.ClassUpdateDto;
+import com.devinhouse.pcpbackend.model.ClassEntity;
+import com.devinhouse.pcpbackend.model.ModuleEntity;
+import com.devinhouse.pcpbackend.model.TeacherEntity;
+import com.devinhouse.pcpbackend.model.WeekEntity;
 import com.devinhouse.pcpbackend.repository.ClassRepository;
+import com.devinhouse.pcpbackend.repository.TeacherRepository;
+import com.devinhouse.pcpbackend.repository.WeekRepository;
 import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
@@ -10,4 +28,49 @@ import org.springframework.stereotype.Service;
 public class ClassService {
 
     private ClassRepository classRepository;
+
+    private WeekRepository weekRepository;
+
+    private TeacherRepository teacherRepository;
+
+    private ModelMapper modelMapper;
+    
+    @Transactional
+    public void save(ClassEntity classEntity) {
+    	classRepository.save(classEntity);
+    	
+    }
+    
+    public List<ClassEntity> getAll() {
+    	return classRepository.findAll();
+    }
+
+    public ClassUpdateDto allocatteTeacher(Long idClass, Long idWeek, Long idTeacher) {
+        try{
+            Optional<ClassEntity> classResponse = classRepository.findById(idClass);
+            if(classResponse.isPresent()){
+                Optional<WeekEntity> weekResponse = weekRepository.findById(idWeek);
+                for(ModuleEntity module : classResponse.get().getModuleEntityList()){
+                    List<WeekEntity> weekEntityList = module.getWeekEntityList();
+                    if(weekResponse.isPresent() &&
+                            weekEntityList.contains(weekResponse.get()) &&
+                            teacherRepository.findById(idTeacher).isPresent()){
+                        Optional<TeacherEntity> teacherResponse = teacherRepository.findById(idTeacher);
+                        weekResponse.get().setTeacherEntity(teacherResponse.get());
+                    }
+                }
+            }
+            return modelMapper.map(classResponse.get(), ClassUpdateDto.class);
+        }catch (Exception e){
+            throw ApiException.entityNotFoundException(DefaultMessageHelper.getMessage(DefaultMessageConstants.ENTITY_NOT_FOUND));
+        }
+    }
+
+    public ClassUpdateDto findById(Long id) {
+        Optional<ClassEntity> classResponse = classRepository.findById(id);
+        if(classResponse.isEmpty()){
+            throw ApiException.entityNotFoundException(DefaultMessageHelper.getMessage(DefaultMessageConstants.ENTITY_NOT_FOUND));
+        }
+        return modelMapper.map(classResponse.get(), ClassUpdateDto.class);
+    }
 }
