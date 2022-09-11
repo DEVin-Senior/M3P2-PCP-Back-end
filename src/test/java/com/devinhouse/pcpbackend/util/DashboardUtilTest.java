6eb6c757f6223c31d1b7af875e0f608d25444e9b
Wagner Pedroso <95456109+wagnerpedroso@users.noreply.github.com>
2022-09-11 19:17:24 -0300

    #SQ01-12 - Teste dela de dashboard
    Teste de dashboard

---
+++ src/test/java/com/devinhouse/pcpbackend/util/DashboardUtilTest.java
@@ -0,0 +1,12 @@
+package com.devinhouse.pcpbackend.util;
+
+ import com.devinhouse.pcpbackend.dto.DashboardDto;
+
+public class DashboardUtilTest {
+    public static DashboardDto expectedDashboard(){
+        DashboardDto expectedDashboard = new DashboardDto();
+        expectedDashboard.toString();
+        return expectedDashboard;
+
+    }
+}
